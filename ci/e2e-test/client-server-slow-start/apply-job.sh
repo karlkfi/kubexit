dee86b8e2965153e9204c7c8b3988eed080789da
Karl Isenberg <karlkfi@gmail.com>
2020-08-20 17:35:50 -0700

    Use explicit git short sha image tag

--- ci/e2e-test/client-server-slow-start/apply-job.sh
+++ ci/e2e-test/client-server-slow-start/apply-job.sh
@@ -4,5 +4,11 @@ set -o errexit -o nounset -o pipefail -o posix
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-kustomize edit set image karlkfi/kubexit=karlkfi/kubexit:latest
+IMAGE_TAG=latest
+if [[ -n "${GITHUB_SHA:-}" ]]; then
+  IMAGE_TAG="sha-${GITHUB_SHA:0:7}"
+fi
+
+echo "Image: karlkfi/kubexit:${IMAGE_TAG}"
+kustomize edit set image "karlkfi/kubexit=karlkfi/kubexit:${IMAGE_TAG}"
 kustomize build . | kubectl apply -f -
